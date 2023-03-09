1e1832f7db1fe0c73bd0006cebe2d5bddb42b286
HaiPhong <haiphong.huynh@gmail.com>
2023-03-09 18:51:40 +0700

    chapter_7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
-    def new; end
+    def new
+        @user = User.new
+    end
+    def show
+        @user = User.find_by id: params[:id]
+        return if @user
+        flash[:warning] = "Not found user!"
+        redirect_to root_path 
+    end
+    def create
+        @user = User.new user_params # Not the final implementation!
+        if @user.save
+        # Handle a successful save.
+        flash[:success] = "User create successful"
+        redirect_to @user
+        else
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit :name, :email, :password, :password_confirmation
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get 'users/new'
   get '/signup', to: 'users#new'
   root 'static_pages#home'
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: %i(new create show)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
