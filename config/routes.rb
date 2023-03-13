868bf464f3ff5a42ea10533a780388c68da8c08c
HaiPhong <haiphong.huynh@gmail.com>
2023-03-13 17:22:35 +0700

    chapter_8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :products
   get 'partials/new'
   get 'partials/edit'
@@ -12,6 +15,9 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   resources :users, only: %i(new create show)
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
