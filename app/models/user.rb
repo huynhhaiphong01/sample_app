fa6d188feaf34aad713598bc2ae64aaded9f08db
HaiPhong <haiphong.huynh@gmail.com>
2023-03-15 14:24:39 +0700

    chapter_9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     before_save :downcase_email
     validates :email, presence: true, length: {maximum: 50},
@@ -6,21 +7,43 @@ class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 50}
 
     has_secure_password
+   
+
+    class << self
+        def digest string
+            cost = if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+            BCrypt::Password.create string, cost: cost
+        end
+
+        def new_token
+        SecureRandom.urlsafe_base64
+        end
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update remember_digest: User.digest(remember_token)
+    end
+    def authenticated? remember_token
+        BCrypt::Password.new(remember_digest).is_password? remember_token
+    end
+
+    # Forgets a user.
+    def forget
+        update_attribute :remember_digest, nil
+    end
+
     private
 
     def downcase_email
         self.email.downcase!
     end
 
-    # Returns the hash digest of the given string.
-    def User.digest string
-        cost = if ActiveModel::SecurePassword.min_cost
-                    BCrypt::Engine::MIN_COST
-            else
-                BCrypt::Engine.cost
-            end
-            BCrypt::Password.create string, cost: cost
-    end
+
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_09_080251) do
+ActiveRecord::Schema.define(version: 2023_03_15_062455) do
 
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2023_03_09_080251) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
